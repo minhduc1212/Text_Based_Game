c926bf45aebbfee30418966710f1bc765b37801b
minhduc1212 <minhduc6e12@gmail.com>
2025-12-30 00:50:35 +0700

    feat(design.drawio, character_class.py):first step to build class

--- src/character_class.py
+++ src/character_class.py
@@ -1,3 +1,6 @@
 class base_class:
-    def __init__(self, name):
+    def __init__(self, name: str, fixed_stat, flex_stat):
         self.name = name
+        self.fixed_stat = []
+        self.flex_stat = []
+    
