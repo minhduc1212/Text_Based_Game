94f606d82cb71e069c41942a11b7c20371003ce4
minhduc1212 <minhduc6e12@gmail.com>
2025-12-31 01:25:52 +0700

    feat(design.drawio, character_class.py): pdate some func of character_class

--- src/character_class.py
+++ src/character_class.py
@@ -1,6 +1,28 @@
+import json
+
 class base_class:
     def __init__(self, name: str, fixed_stat, flex_stat):
         self.name = name
         self.fixed_stat = []
         self.flex_stat = []
+        
+    def add_point(self, stat: str):
+        if stat in self.flex_stat:
+            stat.point += 1
     
+    def lose_point(self, stat: str):
+        if stat in self.flex_stat:
+            stat.point -= 1
+    
+    def load_data(self):
+        with open("class_stat.json", "r") as f:
+            class_data = json.load(f)
+            return class_data[self.name]
+    
+    
+
+class swordman(base_class):
+    def __init__(self, name: str, fixed_stat, flex_stat):
+        super().__init__(name, fixed_stat, flex_stat)       
+        self.fixed_stat = ["mind", "intelligence"]
+        self.flex_stat = ["strength", "dexterity", "vitality", "luck"]
--- src/stat.py
+++ src/stat.py
@@ -1,3 +1,33 @@
 class base_stat:
-    def __init__(self, name):
+    def __init__(self, name: str, point: int):
         self.name = name
+        self.point - point
+        
+class strength(base_stat):
+    def __init__(self, name: str, point: int):
+        super().__init__(name, point)
+        
+class dexterity(base_stat):
+    def __init__(self, name: str, point: int):
+        super().__init__(name, point)
+
+class intelligence(base_stat):
+    def __init__(self, name: str, point: int):
+        super().__init__(name, point)
+
+class dexterity(base_stat):
+    def __init__(self, name: str, point: int):
+        super().__init__(name, point)
+
+class mind(base_stat):
+    def __init__(self, name: str, point: int):
+        super().__init__(name, point)
+        
+class luck(base_stat):
+    def __init__(self, name: str, point: int):
+        super().__init__(name, point)
+
+class vitality(base_stat):
+    def __init__(self, name: str, point: int):
+        super().__init__(name, point)
+    
