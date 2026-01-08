ff0fccb7e9da7e3a631e900fb4d68bfea00640b7
minhduc1212 <minhduc6e12@gmail.com>
2026-01-09 00:04:22 +0700

    add start to create the begin of everything

---
+++ src/start.py
@@ -0,0 +1,23 @@
+def init_add_stat_points(player, stat, points):
+    print("You have 5 points to add to your class stats.")
+    print("What stats do you want to add points to?")
+    print("1. Strength")
+    print("2. Dexterity")
+    print("3. Intelligence")
+    print("4. Mind")
+    print("5. Luck")
+    print("6. Vitality")
+
+    choice = input("Enter the number of your choice: ")
+    if choice == "1":
+        player.character_class.strength.add_point(1)
+    elif choice == "2":
+        player.character_class.dexterity.add_point(1)
+    elif choice == "3":
+        player.character_class.intelligence.add_point(1)
+    elif choice == "4":
+        player.character_class.mind.add_point(1)
+    elif choice == "5":
+        player.character_class.luck.add_point(1)
+    elif choice == "6":
+        player.character_class.vitality.add_point(1)
