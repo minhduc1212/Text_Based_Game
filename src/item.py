043ec80cc9bb26d9ece973edfce227b455b56591
minhduc1212 <minhduc6e12@gmail.com>
2026-03-17 20:43:11 +0700

    feat(item): init of item: Bottle

---
+++ src/item.py
@@ -0,0 +1,6 @@
+class Bottle:
+    def __init__(self, name: str, description: str, quantity: int):
+        self.name = name
+        self.description = description
+        self.quantity = quantity
+        
